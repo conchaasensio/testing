90942e81d73343ab6f970a54c02f82bfbc0a8913
Concha Asensio <conchaasensiomr@gmail.com>
2020-09-08 17:09:03 +0200

    add fizzbuzz solution

--- fizz-buzz/src/fizz_buzz.js
+++ fizz-buzz/src/fizz_buzz.js
@@ -1,5 +1,41 @@
 class FizzBuzz {
+  getNumbers() {
+    let numbers = [];
+    for (let index = 1; index < 101; index++) {
+      const fizzValue = this.calculateFizzValue(index);
+      numbers.push(fizzValue);
+    }
 
+    return numbers;
+  }
+
+  calculateFizzValue(index) {
+    if (this.isFizz(index) && this.isBuzz(index)) {
+      return 'fizzbuzz';
+    } else if (this.isFizz(index)) {
+      return 'fizz';
+    } else if (this.isBuzz(index)) {
+      return 'buzz';
+    } else {
+      return index;
+    }
+  }
+
+  isBuzz(index) {
+    return index % 5 === 0 || this.contain5(index);
+  }
+
+  isFizz(index) {
+    return index % 3 === 0 || this.contain3(index);
+  }
+
+  contain5(index) {
+    return index.toString().includes('5');
+  }
+
+  contain3(index) {
+    return index.toString().includes('3');
+  }
 }
 
 module.exports.FizzBuzz = FizzBuzz;
--- fizz-buzz/test/fizz_buzz.test.js
+++ fizz-buzz/test/fizz_buzz.test.js
@@ -1,9 +1,102 @@
-let {FizzBuzz} = require('../src/fizz_buzz');
+let { FizzBuzz } = require('../src/fizz_buzz');
 
-describe('FizzBuzz', function() {
-  it('change_this_name', function() {
-    let fizzBuzz = new FizzBuzz();
+describe('FizzBuzz', function () {
+  let fizzBuzz;
+  beforeEach(() => {
+    fizzBuzz = new FizzBuzz();
+  });
 
-    expect(true).toBe(true);
+  it('return number 1', function () {
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers[0]).toBe(1);
+  });
+
+  it('return number 2', function () {
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers[1]).toBe(2);
+  });
+
+  it('return a number 4', function () {
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers[3]).toBe(4);
+  });
+
+  it('return an array of 100 numbers', function () {
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers.length).toBe(100);
+  });
+
+  describe('is fizz when ', () => {
+    it('is multiple of 3', function () {
+      let numbers = fizzBuzz.getNumbers();
+
+      expect(numbers[2]).toBe('fizz');
+      expect(numbers[5]).toBe('fizz');
+      expect(numbers[8]).toBe('fizz');
+    });
+
+    it('contains 3', function () {
+      let numbers = fizzBuzz.getNumbers();
+
+      expect(numbers[12]).toBe('fizz');
+      expect(numbers[22]).toBe('fizz');
+      expect(numbers[42]).toBe('fizz');
+    });
+  });
+
+  it('return buzz when is multiple of 5', function () {
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers[4]).toBe('buzz');
+    expect(numbers[9]).toBe('buzz');
+    expect(numbers[19]).toBe('buzz');
+  });
+
+  it('return fizzbuzz when is multiple of 15', function () {
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers[14]).toBe('fizzbuzz');
+    expect(numbers[29]).toBe('fizzbuzz');
+    expect(numbers[44]).toBe('fizzbuzz');
+  });
+
+  it('return name buzz position 56', function () {
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers[55]).toBe('buzz');
+  });
+
+  it('return name buzz position 52', function () {
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers[51]).toBe('buzz');
+  });
+
+  it('return name buzz position 54', function () {
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers[53]).toBe('fizzbuzz');
+  });
+
+  it('return name fizzbuzz position 35', function () {
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers[34]).toBe('fizzbuzz');
+  });
+
+  it('return name fizzbuzz position 53', function () {
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers[52]).toBe('fizzbuzz');
+  });
+
+  it('return name fizzbuzz position 57', function () {
+    let numbers = fizzBuzz.getNumbers();
+
+    expect(numbers[56]).toBe('fizzbuzz');
   });
 });
