9f8eb64bdda53db62a5923e75ef6e42f9409e382
Concha Asensio <conchaasensiomr@gmail.com>
2020-09-08 17:17:27 +0200

    add password validator solution

--- password-validator/src/password_validator.js
+++ password-validator/src/password_validator.js
@@ -1,6 +1,18 @@
 class PasswordValidator {
-  changeThisName() {
-    return true;
+  isValid(password) {
+    return (
+      this.hasMoreThan8Characters(password) &&
+      this.containsACapitalLetter(password) &&
+      /[a-z]/.test(password) &&
+      /[0-9]/.test(password) &&
+      /[_]/.test(password)
+    );
+  }
+  containsACapitalLetter(password) {
+    return /[A-Z]/.test(password);
+  }
+  hasMoreThan8Characters(password) {
+    return password.length > 8;
   }
 }
 
--- password-validator/test/password_validator.test.js
+++ password-validator/test/password_validator.test.js
@@ -1,9 +1,59 @@
 let PasswordValidator = require('../src/password_validator').PasswordValidator;
 
-describe('Password validator', function() {
-  it('change_this_name', function() {
+describe('Password validator', function () {
+  // it('contains one character', function () {
+  //   let passwordValidator = new PasswordValidator();
+
+  //   let isValid = passwordValidator.isValid('A');
+
+  //   expect(isValid).toBe(false);
+  // });
+
+  it('contains less than 9 characters', function () {
     let passwordValidator = new PasswordValidator();
 
-    expect(passwordValidator.changeThisName()).toBe(true);
+    let isValid = passwordValidator.isValid('A3aAAA_A');
+
+    expect(isValid).toBe(false);
+  });
+
+  it('contains at least nine characters', function () {
+    let passwordValidator = new PasswordValidator();
+
+    let isValid = passwordValidator.isValid('--a-A_--3');
+
+    expect(isValid).toBe(true);
+  });
+
+  it('contains at least 1 capital letter', function () {
+    let passwordValidator = new PasswordValidator();
+
+    let isValid = passwordValidator.isValid('-a-a--3--_');
+
+    expect(isValid).toBe(false);
+  });
+
+  it('contains at least 1 lowercase', function () {
+    let passwordValidator = new PasswordValidator();
+
+    let isValid = passwordValidator.isValid('-A--_--3--');
+
+    expect(isValid).toBe(false);
+  });
+
+  it('contains at least 1 number', function () {
+    let passwordValidator = new PasswordValidator();
+
+    let isValid = passwordValidator.isValid('-A--_-a---');
+
+    expect(isValid).toBe(false);
+  });
+
+  it('contains an underscore', function () {
+    let passwordValidator = new PasswordValidator();
+
+    let isValid = passwordValidator.isValid('-A---a-3-');
+
+    expect(isValid).toBe(false);
   });
 });
